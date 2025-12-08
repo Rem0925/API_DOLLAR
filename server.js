d60db73f83b8be5f991c937eb3fc8e7bb3f60cf0
Rem0925 <144285837+Rem0925@users.noreply.github.com>
2025-12-08 15:37:23 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configurar EJS como motor de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -18,26 +17,34 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/dolar/ves', async (req, res) => {
-    const monto_usd = req.query.Usd ?  parseFloat((req.query.Usd).replace(',', '.').trim()) : 0;
+
+    const monto_usd = req.query.Usd ? parseFloat((req.query.Usd).replace(',', '.').trim()) : 0;
     const monto_bs = req.query.Bs ? parseFloat((req.query.Bs).replace(',', '.').trim()) : 0;
     const monto_usdt = req.query.Usdt ? parseFloat((req.query.Usdt).replace(',', '.').trim()) : 0;
     const monto_eu = req.query.Eu ? parseFloat((req.query.Eu).replace(',', '.').trim()) : 0;
 
-    if ((req.query.Usd && isNaN(monto_usd)) && (req.query.Bs && isNaN(monto_bs)) && (req.query.Usdt && isNaN(monto_usdt)) && (req.query.Eu && isNaN(monto_eu))) {
-        return res.status(400).json({ error: "El parámetro 'montoUsd, montoBs, montoUsdt, montoEu' debe ser un número." });
+
+    if ((req.query.Usd && isNaN(monto_usd)) || 
+        (req.query.Bs && isNaN(monto_bs)) || 
+        (req.query.Usdt && isNaN(monto_usdt)) || 
+        (req.query.Eu && isNaN(monto_eu))) {
+        return res.status(400).json({ error: "Los parámetros de monto deben ser números válidos." });
     }
 
-    const resultado = await obtenerPrecioDolar(monto_usd,monto_bs,monto_usdt,monto_eu);
+    const resultado = await obtenerPrecioDolar(monto_usd, monto_bs, monto_usdt, monto_eu);
 
     if (resultado.error) {
         return res.status(500).format({
-            html: () => res.render('index', { precios: {}, error: resultado.error }),
-            json: () => res.json(resultado)
+            json: () => res.json(resultado),
+            html: () => res.render('index', { precios: {}, error: resultado.error })
         });
     }
 
     res.format({
-        // Si es un Navegador web -> Muestra la interfaz HTML (Bootstrap)
+        json: function () {
+            res.status(200).json(resultado);
+        },
+        
         html: function () {
             res.render('index', { 
                 precios: resultado, 
@@ -45,12 +52,6 @@ app.get('/api/dolar/ves', async (req, res) => {
             });
         },
         
-        // Si es la App Móvil o Postman -> Muestra el JSON
-        json: function () {
-            res.status(200).json(resultado);
-        },
-        
-        // Por defecto -> JSON
         default: function () {
             res.status(200).json(resultado);
         }
@@ -62,5 +63,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 API lista en http://localhost:${PORT}/api/dolar/ves o https://api-dollar-0f0i.onrender.com/api/dolar/ves`);
+    console.log(`🚀 API lista en http://localhost:${PORT}/api/dolar/ves`);
 });
